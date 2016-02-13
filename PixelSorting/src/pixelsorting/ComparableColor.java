9663918c0e0d32675c50da32d9e8b9a8569546b0
kvloxx <kvloxx@gmail.com>
2016-02-12 20:14:12 -0500

    Color comparator functions rewritten as lambda expressions ComparableColor no longer implements comparable sortRow fixed as to no longer replicate pixels at end of row

--- PixelSorting/src/pixelsorting/ComparableColor.java
+++ PixelSorting/src/pixelsorting/ComparableColor.java
@@ -1,19 +1,83 @@
 package pixelsorting;
 
+import java.util.Comparator;
+
 import processing.core.PApplet;
 
-public class ComparableColor implements Comparable<ComparableColor> {
-	PApplet p;
+public class ComparableColor{
+	static PApplet p;
 	public int c;
+	// public final Comparator<ComparableColor> COMP_HUE, COMP_BRI, COMP_R,
+	// COMP_G, COMP_B;
+	
+	public static <T> Comparator<T> newMultiComp(Comparator<T> comp1, Comparator<T> comp2, Comparator<T> comp3){
+		Comparator<T> res=(T o1, T o2) ->{
+			int r1=comp1.compare(o1,o2);
+			
+			if(r1!=0)
+				return r1;
+			else
+			{
+				int r2=comp2.compare(o1,o2);
+				
+				if(r2!=0)
+					return r2;
+				else
+				{
+					return comp3.compare(o1, o2);
+				}
+			}
+		};
+		return res;
+	}
+	
+/*	public int comp(Runnable test, T o1, T o2){
+		if (p.hue(thisColor) > p.hue(otherColor))
+			return 1;
+		else if (p.hue(thisColor) < p.hue(otherColor))
+			return -1;
+		else 
+			return 0;
+	}*/
 
-	public ComparableColor(PApplet p, int c) {
+	public final static Comparator<ComparableColor> COMP_HUE = (ComparableColor c1, ComparableColor c2) -> {
+		int thisColor = c1.c;
+		int otherColor = c2.c;
+		if (p.hue(thisColor) > p.hue(otherColor))
+			return 1;
+		else if (p.hue(thisColor) < p.hue(otherColor))
+			return -1;
+		else 
+			return 0;
+	};
+	public final static Comparator<ComparableColor> COMP_SAT = (ComparableColor c1, ComparableColor c2) -> {
+		int thisColor = c1.c;
+		int otherColor = c2.c;
+		if (p.saturation(thisColor) > p.saturation(otherColor))
+			return 1;
+		else if (p.saturation(thisColor) < p.saturation(otherColor))
+			return -1;
+		else 
+			return 0;
+	};
+	public final static Comparator<ComparableColor> COMP_BRI = (ComparableColor c1, ComparableColor c2) -> {
+		int thisColor = c1.c;
+		int otherColor = c2.c;
+		if (p.brightness(thisColor) > p.brightness(otherColor))
+			return 1;
+		else if (p.brightness(thisColor) < p.brightness(otherColor))
+			return -1;
+		else 
+			return 0;
+	};
+
+	public ComparableColor(PApplet _p, int c) {
 		this.c = c;
-		this.p = p;
+		p = _p;
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return c + "";
 	}
 
@@ -38,33 +102,6 @@ public class ComparableColor implements Comparable<ComparableColor> {
 	 * return 0; } } } }
 	 */
 
-	/////////////////////// // HSB SORT // ///////////////////////
-	public int compareTo(ComparableColor cc) {
-		int thisColor = this.c;
-		int otherColor = cc.c;
-
-		if (p.hue(thisColor) > p.hue(otherColor))
-			return 1;
-		else if (p.hue(thisColor) < p.hue(otherColor))
-			return -1;
-		else {
-			if (p.saturation(thisColor) > p.saturation(otherColor))
-				return 1;
-			else if (p.saturation(thisColor) < p.saturation(otherColor))
-				return -1;
-			else {
-				if (p.red(thisColor) > p.red(otherColor))
-					return 1;
-				else if (p.red(thisColor) < p.red(otherColor))
-					return -1;
-				else {
-					return 0;
-				}
-			}
-		}
-	}
-	
-
 	/*
 	 * /////////////////////// // RBG SORT // /////////////////////// public int
 	 * compareTo(ComparableColor cc) { int thisColor = this.c; int otherColor =
@@ -76,4 +113,32 @@ public class ComparableColor implements Comparable<ComparableColor> {
 	 * (p.blue(thisColor) < p.blue(otherColor)) return -1; else { return 0; } }
 	 * } }
 	 */
+
+	/////////////////////// // HSB SORT // ///////////////////////
+
+//	public int compareTo(ComparableColor cc) {
+//		int thisColor = this.c;
+//		int otherColor = cc.c;
+//
+//		if (p.hue(thisColor) > p.hue(otherColor))
+//			return 1;
+//		else if (p.hue(thisColor) < p.hue(otherColor))
+//			return -1;
+//		else {
+//			if (p.saturation(thisColor) > p.saturation(otherColor))
+//				return 1;
+//			else if (p.saturation(thisColor) < p.saturation(otherColor))
+//				return -1;
+//			else {
+//				if (p.red(thisColor) > p.red(otherColor))
+//					return 1;
+//				else if (p.red(thisColor) < p.red(otherColor))
+//					return -1;
+//				else {
+//					return 0;
+//				}
+//			}
+//		}
+//	}
+	
 }
--- PixelSorting/src/pixelsorting/PixelSorting.java
+++ PixelSorting/src/pixelsorting/PixelSorting.java
@@ -10,17 +10,17 @@ public class PixelSorting extends PApplet {
 	PImage img;
 	// String imgFileName = "glitch_art_small";
 	// String imgFileName = "outputthing";
-	// String imgFileName = "cat";
-	String imgFileName = "dumbDolphinCropped";
+	String imgFileName = "cat";
+	// String imgFileName = "dumbDolphinCropped";
 	String fileType = "jpg";
-	
+
 	int row = 0;
 	int column = 0;
 
 	boolean saved = false;
 
 	public void settings() {
-		size(470, 711);
+		size(10,10);
 	}
 
 	public void setup() {
@@ -30,7 +30,7 @@ public class PixelSorting extends PApplet {
 
 	public void draw() {
 
-		rangeSortRow(10);
+		rangeSortRow(5);
 		image(img, 0, 0);
 		// noLoop();
 	}
@@ -44,7 +44,7 @@ public class PixelSorting extends PApplet {
 			unsorted.add(i, new ComparableColor(this, img.pixels[i + row * img.width]));
 
 		}
-		Collections.sort(unsorted);
+		// Collections.sort(unsorted);
 		unsorted.toArray(sorted);
 		for (int i = 0; i < img.width; i++)
 			img.pixels[i + row * img.width] = sorted[i].c;
@@ -60,76 +60,20 @@ public class PixelSorting extends PApplet {
 			}
 
 			for (int x = 0; x < img.width; x++) {
-				Collections.sort(subseq);
+				// Collections.sort(subseq, ComparableColor.COMP_HUE);
+				Collections.sort(subseq, ComparableColor.newMultiComp(ComparableColor.COMP_BRI,
+						ComparableColor.COMP_HUE, ComparableColor.COMP_BRI));
 				img.pixels[row * img.width + x] = subseq.get(0).c;
-				subseq.set(0, new ComparableColor(this, img.pixels[row * img.width + (x + range) % img.width]));
+				if ((x + range) < img.width)
+					subseq.set(0, new ComparableColor(this, img.pixels[row * img.width + (x + range) % img.width]));
+				else
+					subseq.remove(0);
 			}
 		}
 		img.updatePixels();
 		image(img, 0, 0);
 	}
 
-	// void rangeSortRow(int range) {
-	// img.loadPixels();
-	// for (int row = 0; row < img.height; row++) {
-	// ArrayList<ComparableColor> subsort = new
-	// ArrayList<ComparableColor>(range);
-	//
-	// for (int x = 0; x < img.width; x++) {
-	// for (int i = 0; i < range; i++) {
-	// subsort.add(new ComparableColor(this, img.pixels[row * img.width + (x +
-	// i) % img.width]));
-	// }
-	// Collections.sort(subsort);
-	//// for (int i = 0; i < range; i++) {
-	//// img.pixels[row * img.width + (x + i) % img.width]=subsort.get(i).c;
-	//// }
-	// img.pixels[row * img.width + x]=subsort.get(0).c;
-	// }
-	// }
-	// img.updatePixels();
-	// image(img, 0, 0);
-	// }
-
-	// void rangeSortRow(int range) {
-	// int x = 0;
-	// row = 0;
-	// if (range > img.width) {
-	// System.out.println("Error: sorting range must be less than width of row
-	// for rangeSort");
-	// return;
-	// }
-	//
-	// while (row < img.height - 1) {
-	// ComparableColor[] sorted = new ComparableColor[range];
-	//
-	// for (x = 0; x < img.width; x++) {
-	// ArrayList<ComparableColor> unsorted = new
-	// ArrayList<ComparableColor>(range);
-	// img.loadPixels();
-	//
-	// for (int i = 0; i < range; i++)
-	// unsorted.add(i, new ComparableColor(this, img.pixels[row * img.width + (x
-	// + i) % img.width]));
-	//
-	// Collections.sort(unsorted);
-	// unsorted.toArray(sorted);
-	//
-	// for (int i = 0; i < range; i++)
-	// img.pixels[row * img.width + (x + i) % img.width] = sorted[i].c;
-	// img.updatePixels();
-	// }
-	//
-	// row++;
-	// image(img, 0, 0);
-	// }
-	//
-	// if (!saved) {
-	// saveFrame();
-	// saved = true;
-	// }
-	//
-	// }
 	public static void main(String _args[]) {
 		PApplet.main(new String[] { pixelsorting.PixelSorting.class.getName() });
 	}
