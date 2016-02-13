631e5fe2d7d73dad7fb62379737c3b9e9fcd931d
kvloxx <kvloxx@gmail.com>
2016-02-13 00:52:26 -0500

    Bye bye ComparableColor class.
    no compColor, slicker usage of Comparator class for sorting

--- PixelSorting/src/pixelsorting/PixelSorting.java
+++ PixelSorting/src/pixelsorting/PixelSorting.java
@@ -2,6 +2,8 @@ package pixelsorting;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.function.IntUnaryOperator;
 
 import processing.core.*;
 
@@ -10,7 +12,8 @@ public class PixelSorting extends PApplet {
 	PImage img;
 	// String imgFileName = "glitch_art_small";
 	// String imgFileName = "outputthing";
-	String imgFileName = "cat";
+	// String imgFileName = "cat";
+	String imgFileName = "Be-a-Communist-Step-20";
 	// String imgFileName = "dumbDolphinCropped";
 	String fileType = "jpg";
 
@@ -20,7 +23,7 @@ public class PixelSorting extends PApplet {
 	boolean saved = false;
 
 	public void settings() {
-		size(10,10);
+		size(10, 10);
 	}
 
 	public void setup() {
@@ -32,40 +35,42 @@ public class PixelSorting extends PApplet {
 
 		rangeSortRow(5);
 		image(img, 0, 0);
+		if (frameCount % 2 == 0 && frameCount < 150)
+			saveFrame(frameCount + ".jpg");
 		// noLoop();
 	}
 
 	void sortRow(int row) {
 
-		ArrayList<ComparableColor> unsorted = new ArrayList<ComparableColor>(img.width);
-		ComparableColor[] sorted = new ComparableColor[img.width];
+		ArrayList<Integer> unsorted = new ArrayList<Integer>(img.width);
 
 		for (int i = 0; i < img.width; i++) {
-			unsorted.add(i, new ComparableColor(this, img.pixels[i + row * img.width]));
-
+			unsorted.add(i,img.pixels[i + row * img.width]);
 		}
-		// Collections.sort(unsorted);
-		unsorted.toArray(sorted);
+		Collections.sort(unsorted);
 		for (int i = 0; i < img.width; i++)
-			img.pixels[i + row * img.width] = sorted[i].c;
+			img.pixels[i + row * img.width] = unsorted.get(i);
 	}
 
 	void rangeSortRow(int range) {
 		img.loadPixels();
 		for (int row = 0; row < img.height; row++) {
-			ArrayList<ComparableColor> subseq = new ArrayList<ComparableColor>(range);
+			ArrayList<Integer> subseq = new ArrayList<>(range);
 
 			for (int i = 0; i < range; i++) { // prime the subsequence array
-				subseq.add(new ComparableColor(this, img.pixels[row * img.width + i]));
+				subseq.add(img.pixels[row * img.width + i]);
 			}
 
 			for (int x = 0; x < img.width; x++) {
-				// Collections.sort(subseq, ComparableColor.COMP_HUE);
-				Collections.sort(subseq, ComparableColor.newMultiComp(ComparableColor.COMP_BRI,
-						ComparableColor.COMP_HUE, ComparableColor.COMP_BRI));
-				img.pixels[row * img.width + x] = subseq.get(0).c;
+				// Collections.sort(subseq,
+				// Comparator.comparing(this::hue).thenComparing(this::saturation)
+				// .thenComparing(this::brightness));
+
+				Collections.sort(subseq, Comparator.comparing(this::red));
+				// Collections.sort(subseq, Comparator.comparing(this::hue));
+				img.pixels[row * img.width + x] = subseq.get(0);
 				if ((x + range) < img.width)
-					subseq.set(0, new ComparableColor(this, img.pixels[row * img.width + (x + range) % img.width]));
+					subseq.set(0, img.pixels[row * img.width + (x + range) % img.width]);
 				else
 					subseq.remove(0);
 			}
